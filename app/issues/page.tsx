5c66726ff1920dbf4b3bb4650b1f5b0a66be8ec9
sunnng <61936492+sunnng@users.noreply.github.com>
2024-01-30 22:44:11 +0800

    Build the new issue page

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,8 +1,18 @@
+"use client";
+
 import React from "react";
-import { Button } from "@radix-ui/themes";
+import { Button, TextField, TextArea } from "@radix-ui/themes";
 
 const IssuesPage = () => {
-  return <Button>Let's go</Button>;
+  return (
+    <div className="flex justify-center flex-col space-y-3 max-w-xl m-auto">
+      <TextField.Root>
+        <TextField.Input placeholder="Title" />
+      </TextField.Root>
+      <TextArea placeholder="Description…" />
+      <Button>Submit New Issue</Button>
+    </div>
+  );
 };
 
 export default IssuesPage;
